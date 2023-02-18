d59c6eb9faaee1aad65efc8f578cf9c2d272ed25
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-18 14:13:14 +0500

    Add model

---
+++ TrackListApp/TrackListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TrackListViewController.swift
+//  TrackListApp
+//
+//  Created by Yuriy on 18.02.2023.
+//
+
+import UIKit
+
+class TrackListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
