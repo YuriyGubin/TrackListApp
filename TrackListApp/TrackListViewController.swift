9b462da5052ef3366d7dcaed3cae625057b0b4be
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-18 14:36:53 +0500

    Work with table view

--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -8,28 +8,39 @@
 import UIKit
 
 class TrackListViewController: UITableViewController {
+    
+    private var trackList = Track.getTrackList()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.rowHeight = 80
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        trackList.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "track", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let track = trackList[indexPath.row]
+        
+        content.text = track.artist
+        content.secondaryText = track.song
+        content.image = UIImage(named: track.title)
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
+        cell.contentConfiguration = content
 
         return cell
     }
     
+//    // MARK: - UITableViewDelegate
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        80
+//    }
 
     
     
